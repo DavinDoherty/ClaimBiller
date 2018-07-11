fdd980da633916ca6a3aecc51612fb993009a40f
Davin <Davin@DESKTOP-A45LMMI>
2018-07-11 19:04:12 +0100

    commit_001

---
+++ index.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <title>Medical Claim Biller</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+    <div id="container">
+        <?php include("includes/header.html");?>
+        <?php include("includes/nav.html");?>
+        <div id="content">
+        
+        
+        </div>
+
+         <div id="footer">
+        <?php include("includes/footer.html");?>
+        </div>
+        
+    </div>
+   
+
+
+    
+</body>
+</html>
+
